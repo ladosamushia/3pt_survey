3cd7c47870bdc830538393da9117ca6ae71f59bf
Lado Samushia <lado.samushia.office@gmail.com>
2021-12-02 12:20:45 -0600

    insignificant

--- scripts/basic.jl
+++ scripts/basic.jl
@@ -6,9 +6,9 @@ using Random
 
 include("../src/triple_loop.jl")
 
-Ngal = 1000000
+Ngal = 2000000
 xyz = rand(3, Ngal)*2000
 w = ones(Ngal)
 
-hist = all_triplets(xyz, w, 0.1, 20, 20, 1, 40, logr=true, logp=false)
+hist = all_triplets(xyz, w, 0.1, 20, 10, 1, 40, logr=true, logp=false)
 sum(hist)
